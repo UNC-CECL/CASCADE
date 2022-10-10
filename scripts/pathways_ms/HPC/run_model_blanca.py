ce9c012b5737662cc0655c0ed55035fffe8a3664
K Anarde <57640439+anardek@users.noreply.github.com>
2022-10-10 12:14:19 -0400

    rename CHOM coupler

--- scripts/pathways_ms/HPC/run_model_blanca.py
+++ scripts/pathways_ms/HPC/run_model_blanca.py
@@ -50,7 +50,7 @@ def RUN_4_CASCADE_noAST_Rave_SLR_pt004_NoHumans(
         roadway_management_module=False,  # no roadway management
         alongshore_transport_module=False,  # no brie coupling
         beach_nourishment_module=False,  # no beach nourishment
-        community_dynamics_module=False,  # no community dynamics
+        community_economics_module=False,  # no community dynamics
     )
 
     # --------- LOOP ---------
