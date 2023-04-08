b6ebc78679aafba9cb97c99729c51c87f113fadb
Lexi Van Blunk <agvanblu@ncsu.edu>
2023-04-08 19:38:22 -0400

    changes to plot figures

--- cascade/lidar_to_array.py
+++ cascade/lidar_to_array.py
@@ -1,15 +1,17 @@
 # conversion of Benton's Lidar to Barrier3D R code to Python
 # some changes have been made to the original code to better suit North Core Banks
 # Lexi Van Blunk
-# 2/16/23
-
+# 2/16/23, updated 4/8/23
+import matplotlib.patches
 import numpy as np
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-plt.rcParams['figure.figsize'] = (8,6)
-plt.rcParams.update({"font.size": 15})
+# plt.rcParams['figure.figsize'] = (10, 6)
+plt.rcParams.update({"font.size": 12})
+# plt.rcParams['figure.constrained_layout.use'] = True
 
 
 # load the data as a dataframe
@@ -41,21 +43,34 @@ for r in range(135):
         if elev_array[r, c] <= 0:
             elev_array[r, c] = -3
 
-# plot the adjusted domain
+# # plot the adjusted domain
 # fig1 = plt.figure()
-# ax1 = fig1.add_subplot(111)
+# ax1 = fig1.add_subplot(121)
 # mat = ax1.matshow(
 #     elev_array,
 #     cmap="terrain",
 #     vmin=-3.0,
 #     vmax=6.0,
 # )
-# cbar = fig1.colorbar(mat)
-# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# # cbar = fig1.colorbar(mat)
+# # cbar.set_label('m MHW', rotation=270, labelpad=15)
 # ax1.set_title("Pre-Storm Elevation")
-# ax1.set_ylabel("barrier width (dam)")
-# ax1.set_xlabel("barrier length (dam)")
+# ax1.set_ylabel("barrier width (m)")
+# ax1.set_xlabel("barrier length (m)")
 # plt.gca().xaxis.tick_bottom()
+# ax1.add_patch(plt.Rectangle((20,175), 50, -40, lw=2, ec="k", fc="none"))
+# ax1.add_patch(plt.Rectangle((70,175), 50, -40, lw=2, ec="m", fc="none"))
+# # ls = linestyle, lw = line width, ec = edge color, fc = fill color
+#
+# xtick_max = np.shape(elev_array)[1]  # n_cols = x
+# x_ticks = np.array(range(0, xtick_max, 25))
+# x_tick_labels = x_ticks * 10
+# ytick_max = np.shape(elev_array)[0]  # n_rows = y
+# y_ticks = np.array(range(0, ytick_max, 25))
+# y_tick_labels = y_ticks * 10
+# plt.xticks(x_ticks, x_tick_labels)
+# plt.yticks(y_ticks, y_tick_labels)
+# fig1.subplots_adjust(wspace=0, hspace=0)
 
 # convert to dam and make our two segments
 elev_array = elev_array / 10
@@ -78,7 +93,7 @@ section2_pre = elev_array[:, 70:120]
 # ax1.set_ylabel("barrier width (dam)")
 # ax1.set_xlabel("barrier length (dam)")
 # plt.gca().xaxis.tick_bottom()
-
+#
 # # plot section 2
 # fig1 = plt.figure()
 # ax1 = fig1.add_subplot(111)
@@ -121,20 +136,32 @@ for r in range(135):
         if post_elev_array[r, c] <= 0:
             post_elev_array[r, c] = -3
 
-# fig2 = plt.figure()
-# ax2 = fig2.add_subplot(111)
+# # fig2 = plt.figure()
+# ax2 = fig1.add_subplot(122)
 # mat = ax2.matshow(
 #     post_elev_array,
 #     cmap="terrain",
 #     vmin=-3.0,
 #     vmax=6.0,
 # )
-# cbar = fig2.colorbar(mat)
+# cbar = fig1.colorbar(mat)
 # cbar.set_label('m MHW', rotation=270, labelpad=15)
 # ax2.set_title("Post-Storm Elevation")
-# ax2.set_ylabel("barrier width (dam)")
-# ax2.set_xlabel("barrier length (dam)")
+# # ax2.set_ylabel("barrier width (m)")
+# ax2.set_xlabel("barrier length (m)")
 # plt.gca().xaxis.tick_bottom()
+# ax2.add_patch(plt.Rectangle((20,175), 50, -40, lw=2, ec="k", fc="none"))
+# ax2.add_patch(plt.Rectangle((70,175), 50, -40, lw=2, ec="m", fc="none"))
+#
+# xtick_max = np.shape(post_elev_array)[1]  # n_cols = x
+# x_ticks = np.array(range(0, xtick_max, 25))
+# x_tick_labels = x_ticks * 10
+# # ytick_max = np.shape(post_elev_array)[0]  # n_rows = y
+# # y_ticks = np.array(range(0, ytick_max, 25))
+# # y_tick_labels = y_ticks * 10
+# plt.xticks(x_ticks, x_tick_labels)
+# ax2.tick_params(left=False)
+# ax2.set(yticklabels=[])
 
 
 # convert to dam
@@ -185,53 +212,89 @@ difference_left = post_left-pre_left
 total_erosion_left = np.sum(difference_left)*10
 back_erosion_left = np.sum(difference_left[3:30, 14:38])*10
 
-# fig2 = plt.figure()
-# ax2 = fig2.add_subplot(111)
-# mat = ax2.matshow(
-#     pre_left*10,
-#     cmap="terrain",
-#     vmin=-3.0,
-#     vmax=6.0,
-# )
-# cbar = fig2.colorbar(mat)
-# cbar.set_label('m MHW', rotation=270, labelpad=15)
-# ax2.set_title("Left Zoom Pre-Storm Elevation")
-# ax2.set_ylabel("barrier width (dam)")
-# ax2.set_xlabel("barrier length (dam)")
-# plt.gca().xaxis.tick_bottom()
-#
-# fig2 = plt.figure()
-# ax2 = fig2.add_subplot(111)
-# mat = ax2.matshow(
-#     post_left*10,
-#     cmap="terrain",
-#     vmin=-3.0,
-#     vmax=6.0,
-# )
-# cbar = fig2.colorbar(mat)
-# cbar.set_label('m MHW', rotation=270, labelpad=15)
-# ax2.set_title("Left Zoom Post-Storm Elevation")
-# ax2.set_ylabel("barrier width (dam)")
-# ax2.set_xlabel("barrier length (dam)")
-# plt.gca().xaxis.tick_bottom()
-#
+shrink=0.4
+
+fig1 = plt.figure()
+fig1.tight_layout()
+fig1.suptitle('Configuration 1: Observed Erosion', weight="bold")
+ax1 = fig1.add_subplot(131)
+mat = ax1.matshow(
+    pre_left*10,
+    cmap="terrain",
+    vmin=-3.0,
+    vmax=6.0,
+)
+cbar = fig1.colorbar(mat, shrink=shrink)
+cbar.set_label('m MHW', rotation=270, labelpad=5)
+ax1.set_title("Pre-Storm Elevation")
+ax1.set_ylabel("barrier width (m)")
+ax1.set_xlabel("barrier length (m)")
+plt.gca().xaxis.tick_bottom()
+xtick_max = np.shape(pre_left)[1]  # n_cols = x
+x_ticks = np.array(range(0, xtick_max, 10))
+x_tick_labels = x_ticks * 10
+ytick_max = np.shape(pre_left)[0]  # n_rows = y
+y_ticks = np.array(range(0, ytick_max, 10))
+y_tick_labels = y_ticks * 10
+plt.xticks(x_ticks, x_tick_labels)
+plt.yticks(y_ticks, y_tick_labels)
+
+
+ax2 = fig1.add_subplot(132)
+mat = ax2.matshow(
+    post_left*10,
+    cmap="terrain",
+    vmin=-3.0,
+    vmax=6.0,
+)
+cbar = fig1.colorbar(mat, shrink=shrink)
+cbar.set_label('m MHW', rotation=270, labelpad=5)
+ax2.set_title("Post-Storm Elevation")
+ax2.set_ylabel("barrier width (m)")
+ax2.set_xlabel("barrier length (m)")
+plt.gca().xaxis.tick_bottom()
+xtick_max = np.shape(post_left)[1]  # n_cols = x
+x_ticks = np.array(range(0, xtick_max, 10))
+x_tick_labels = x_ticks * 10
+ytick_max = np.shape(post_left)[0]  # n_rows = y
+y_ticks = np.array(range(0, ytick_max, 10))
+y_tick_labels = y_ticks * 10
+plt.xticks(x_ticks, x_tick_labels)
+plt.yticks(y_ticks, y_tick_labels)
+# ax2.tick_params(left=False)
+# ax2.set(yticklabels=[])
+# fig2.subplots_adjust(wspace=0.3, hspace=0)
+
 # fig3 = plt.figure()
-# ax3 = fig3.add_subplot(111)
-# mat = ax3.matshow(
-#     difference_left*10,
-#     cmap="seismic",
-#     vmin=-5,
-#     vmax=5,
-# )
-# cbar = fig3.colorbar(mat)
-# cbar.set_label('meters', rotation=270, labelpad=15)
-# ax3.set_title("Difference Map")
-# ax3.set_ylabel("barrier width (dam)")
-# ax3.set_xlabel("barrier length (dam)")
-# plt.gca().xaxis.tick_bottom()
+ax3 = fig1.add_subplot(133)
+mat = ax3.matshow(
+    difference_left*10,
+    cmap="seismic",
+    vmin=-5,
+    vmax=5,
+)
+# Configuration 1 \n
+cbar = fig1.colorbar(mat, shrink=shrink)
+cbar.set_label('meters', rotation=270, labelpad=5)
+ax3.set_title("Elevation Change")
+ax3.set_ylabel("barrier width (m)")
+ax3.set_xlabel("barrier length (m)")
+plt.gca().xaxis.tick_bottom()
+
+ax3.add_patch(plt.Rectangle((13,30), 24, -27, lw=3, ec="k", fc="none"))
+
+xtick_max = np.shape(difference_left)[1]  # n_cols = x
+x_ticks = np.array(range(0, xtick_max, 10))
+x_tick_labels = x_ticks * 10
+ytick_max = np.shape(difference_left)[0]  # n_rows = y
+y_ticks = np.array(range(0, ytick_max, 10))
+y_tick_labels = y_ticks * 10
+plt.xticks(x_ticks, x_tick_labels)
+plt.yticks(y_ticks, y_tick_labels)
+fig1.subplots_adjust(top=1.25, wspace=0.4, hspace=0)
 
 
-# break up the domain
+# break up the domain --------------------------------------------------------------------------------------------------
 section1_pre_int = pre_left[0:30]
 # section1_pre_int = pre_left[0:32]
 # section1_pre_int[24:, 24:27] = 0.085
@@ -250,20 +313,34 @@ section1_pre_beach = pre_left[35:]
 full = np.append(section1_pre_int, section1_pre_dunes, 0)
 full = np.append(full, section1_pre_beach, 0)
 
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-mat = ax2.matshow(
-    full*10,
-    cmap="terrain",
-    vmin=-3.0,
-    vmax=6.0,
-)
-cbar = fig2.colorbar(mat)
-cbar.set_label('m MHW', rotation=270, labelpad=15)
-ax2.set_title("Pre-Storm Elevation")
-ax2.set_ylabel("barrier width (dam)")
-ax2.set_xlabel("barrier length (dam)")
-plt.gca().xaxis.tick_bottom()
+# fig2 = plt.figure()
+# fig2.set_tight_layout
+# ax1 = fig2.add_subplot(121)
+# mat = ax1.matshow(
+#     full*10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# # cbar = fig1.colorbar(mat)
+# # cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax1.set_title("Configuration 1")
+# ax1.set_ylabel("barrier width (m)")
+# ax1.set_xlabel("barrier length (m)")
+# plt.gca().xaxis.tick_bottom()
+# # ax1.add_patch(plt.Rectangle((20,175), 50, -40, lw=2, ec="k", fc="none"))
+# # ax1.add_patch(plt.Rectangle((70,175), 50, -40, lw=2, ec="m", fc="none"))
+# # ls = linestyle, lw = line width, ec = edge color, fc = fill color
+#
+# xtick_max = np.shape(full)[1]  # n_cols = x
+# x_ticks = np.array(range(0, xtick_max, 10))
+# x_tick_labels = x_ticks * 10
+# ytick_max = np.shape(full)[0]  # n_rows = y
+# y_ticks = np.array(range(0, ytick_max, 5))
+# y_tick_labels = y_ticks * 10
+# plt.xticks(x_ticks, x_tick_labels)
+# plt.yticks(y_ticks, y_tick_labels)
+# # fig2.subplots_adjust(wspace=0, hspace=0)
 
 
 interior_b3d_input = np.flip(section1_pre_int)
@@ -277,61 +354,98 @@ dunes_input = np.append(dunes_b3d[0], dunes_b3d[1], 0)
 # the beach should be loaded in with the ocean on the bottom
 # np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-beach-left-dam-test", section1_pre_beach)
 
-# re-formatting the right section
-pre_right = section2_pre[36:76]
-post_right = section2_post[35:75]
 
-difference = post_right-pre_right
-total_erosion_right = np.sum(difference)*10
-back_erosion_right = np.sum(difference[4:28, 16:42])*10
+# re-formatting the right section --------------------------------------------------------------------------------------
+pre_right = section2_pre[34:76]
+post_right = section2_post[33:75]
+
+difference_right = post_right-pre_right
+total_erosion_right = np.sum(difference_right)*10
+back_erosion_right = np.sum(difference_right[4:30, 16:42])*10
+
+shrink=0.4
 
 fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-mat = ax2.matshow(
+fig2.tight_layout()
+fig2.suptitle('Configuration 2: Observed Erosion', weight="bold")
+ax1 = fig2.add_subplot(131)
+mat = ax1.matshow(
     pre_right*10,
     cmap="terrain",
     vmin=-3.0,
     vmax=6.0,
 )
-cbar = fig2.colorbar(mat)
-cbar.set_label('m MHW', rotation=270, labelpad=15)
-ax2.set_title("Right Edited Pre-Storm Elevation")
-ax2.set_ylabel("barrier width (dam)")
-ax2.set_xlabel("barrier length (dam)")
+cbar = fig2.colorbar(mat, shrink=shrink)
+cbar.set_label('m MHW', rotation=270, labelpad=5)
+ax1.set_title("Pre-Storm Elevation")
+ax1.set_ylabel("barrier width (m)")
+ax1.set_xlabel("barrier length (m)")
 plt.gca().xaxis.tick_bottom()
+xtick_max = np.shape(pre_right)[1]  # n_cols = x
+x_ticks = np.array(range(0, xtick_max, 10))
+x_tick_labels = x_ticks * 10
+ytick_max = np.shape(pre_right)[0]  # n_rows = y
+y_ticks = np.array(range(0, ytick_max, 10))
+y_tick_labels = y_ticks * 10
+plt.xticks(x_ticks, x_tick_labels)
+plt.yticks(y_ticks, y_tick_labels)
 
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
+
+ax2 = fig2.add_subplot(132)
 mat = ax2.matshow(
     post_right*10,
     cmap="terrain",
     vmin=-3.0,
     vmax=6.0,
 )
-cbar = fig2.colorbar(mat)
-cbar.set_label('m MHW', rotation=270, labelpad=15)
-ax2.set_title("Right Edited Post-Storm Elevation")
-ax2.set_ylabel("barrier width (dam)")
-ax2.set_xlabel("barrier length (dam)")
+cbar = fig2.colorbar(mat, shrink=shrink)
+cbar.set_label('m MHW', rotation=270, labelpad=5)
+ax2.set_title("Post-Storm Elevation")
+ax2.set_ylabel("barrier width (m)")
+ax2.set_xlabel("barrier length (m)")
 plt.gca().xaxis.tick_bottom()
+xtick_max = np.shape(post_right)[1]  # n_cols = x
+x_ticks = np.array(range(0, xtick_max, 10))
+x_tick_labels = x_ticks * 10
+ytick_max = np.shape(post_right)[0]  # n_rows = y
+y_ticks = np.array(range(0, ytick_max, 10))
+y_tick_labels = y_ticks * 10
+plt.xticks(x_ticks, x_tick_labels)
+plt.yticks(y_ticks, y_tick_labels)
+# ax2.tick_params(left=False)
+# ax2.set(yticklabels=[])
+# fig2.subplots_adjust(wspace=0.3, hspace=0)
 
-fig3 = plt.figure()
-ax3 = fig3.add_subplot(111)
+# fig3 = plt.figure()
+ax3 = fig2.add_subplot(133)
 mat = ax3.matshow(
-    difference*10,
+    difference_right*10,
     cmap="seismic",
     vmin=-5,
     vmax=5,
 )
-cbar = fig3.colorbar(mat)
-cbar.set_label('meters', rotation=270, labelpad=15)
-ax3.set_title("Difference Map")
-ax3.set_ylabel("barrier width (dam)")
-ax3.set_xlabel("barrier length (dam)")
+# Configuration 1 \n
+cbar = fig2.colorbar(mat, shrink=shrink)
+cbar.set_label('meters', rotation=270, labelpad=5)
+ax3.set_title("Elevation Change")
+ax3.set_ylabel("barrier width (m)")
+ax3.set_xlabel("barrier length (m)")
 plt.gca().xaxis.tick_bottom()
 
-section2_pre_int = pre_right[0:28]
-section2_pre_dunes = pre_right[28:32]
+ax3.add_patch(plt.Rectangle((15,30), 25, -24, lw=3, ec="k", fc="none"))
+
+xtick_max = np.shape(difference_right)[1]  # n_cols = x
+x_ticks = np.array(range(0, xtick_max, 10))
+x_tick_labels = x_ticks * 10
+ytick_max = np.shape(difference_right)[0]  # n_rows = y
+y_ticks = np.array(range(0, ytick_max, 10))
+y_tick_labels = y_ticks * 10
+plt.xticks(x_ticks, x_tick_labels)
+plt.yticks(y_ticks, y_tick_labels)
+fig2.subplots_adjust(top=1.25, wspace=0.4, hspace=0)
+
+section2_pre_int = pre_right[0:30]
+section2_pre_dunes = pre_right[30:32]
 section2_pre_dunes[0:2, 0:4] += 0.1
 # section2_pre_dunes[0:2, 0:2] = section2_pre_dunes[3, 0:2]
 # section2_pre_dunes[0, 2:5] = section2_pre_dunes[2, 2:5]
@@ -339,25 +453,46 @@ section2_pre_dunes = section2_pre_dunes[0:2]
 section2_pre_dunes[0, 0:20] += 0.05
 section2_pre_dunes[1, 0:16] += 0.1
 # section2_pre_beach = pre[32:]
-section2_pre_beach = np.load(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data\NCB-default-beach.npy")
+# section2_pre_beach = np.load(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data\NCB-default-beach.npy")
+section2_pre_beach = section1_pre_beach
 
-full = np.append(section2_pre_int, section2_pre_dunes, 0)
-full = np.append(full, section2_pre_beach, 0)
+full_right = np.append(section2_pre_int, section2_pre_dunes, 0)
+full_right = np.append(full_right, section2_pre_beach, 0)
 
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-mat = ax2.matshow(
-    full*10,
-    cmap="terrain",
-    vmin=-3.0,
-    vmax=6.0,
-)
-cbar = fig2.colorbar(mat)
-cbar.set_label('m MHW', rotation=270, labelpad=15)
-ax2.set_title("Pre-Storm Elevation")
-ax2.set_ylabel("barrier width (dam)")
-ax2.set_xlabel("barrier length (dam)")
-plt.gca().xaxis.tick_bottom()
+# ax2 = fig2.add_subplot(122)
+# mat = ax2.matshow(
+#     full_right*10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig2.colorbar(mat, shrink=0.7)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+#
+# ax2.set_title("Configuration 2")
+# # ax2.set_ylabel("barrier width (m)")
+# ax2.set_xlabel("barrier length (m)")
+# plt.gca().xaxis.tick_bottom()
+# fig2.set_figheight(15)
+# # ax2.add_patch(plt.Rectangle((20,175), 50, -40, lw=2, ec="k", fc="none"))
+# # ax2.add_patch(plt.Rectangle((70,175), 50, -40, lw=2, ec="m", fc="none"))
+#
+# xtick_max = np.shape(full_right)[1]  # n_cols = x
+# x_ticks = np.array(range(0, xtick_max, 10))
+# x_tick_labels = x_ticks * 10
+# ytick_max = np.shape(full_right)[0]  # n_rows = y
+# # y_ticks = np.array(range(0, ytick_max, 5))
+# # y_tick_labels = y_ticks * 10
+# plt.xticks(x_ticks, x_tick_labels)
+# # plt.yticks(y_ticks, y_tick_labels)
+# ax2.tick_params(left=False)
+# ax2.set(yticklabels=[])
+
+# divider = make_axes_locatable(ax2)
+# cax = divider.append_axes("right",  size="5%", pad=0.5)
+# plt.colorbar(mat, cax=cax, orientation='vertical', pad=15, label="m MHW")
+
+
 
 interior_b3d_input = np.flip(section2_pre_int)
 # np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-elevation-right-damMHW-test", interior_b3d_input)
