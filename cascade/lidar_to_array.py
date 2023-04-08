6173f348075ce7b15e57c78d7b5e8f702055c955
Lexi Van Blunk <agvanblu@ncsu.edu>
2023-04-07 22:08:17 -0400

    aded more figures to track progress and make sure I was saving everything in the correct untis

--- cascade/lidar_to_array.py
+++ cascade/lidar_to_array.py
@@ -7,66 +7,25 @@ import numpy as np
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
-from collections import Counter
 
-def marsh_averages(
-        elev_array,
-        int_width,
-        length,
-        block_size):
+plt.rcParams['figure.figsize'] = (8,6)
+plt.rcParams.update({"font.size": 15})
 
-    # adjusting for a block size that does not divide evenly into the domain
-    b_size = block_size
-    extra_vert_cells = int_width % b_size  # gives the extra row cells
-    extra_lat_cells = length % b_size  # gives the extra column cells
-
-    # calculating how many times we will shift the block to calculate blocks of average slopes
-    if extra_vert_cells == 0:
-        n_shifts_vert = int(int_width / b_size)
-    else:
-        n_shifts_vert = int((int_width - extra_vert_cells) / b_size) + 1
-    if extra_lat_cells == 0:
-        n_shifts_lat = int(length / b_size)
-    else:
-        n_shifts_lat = int((length - extra_lat_cells) / b_size) + 1
-
-    # Shift through the entire column and then move block up to the next row
-    # start right before the dune line
-
-    for v in range(n_shifts_vert):
-        if v == n_shifts_vert-1 and extra_vert_cells != 0:  # this is the last shift
-            start_row = stop_row
-            stop_row = start_row + extra_vert_cells
-        else:
-            start_row = v * b_size
-            stop_row = v * b_size + b_size
-        for l in range(n_shifts_lat):
-            if l == n_shifts_lat-1 and extra_lat_cells != 0:
-                start_col = end_col
-                end_col = start_col + extra_lat_cells + 1
-            else:
-                start_col = l*b_size
-                end_col = l*b_size + b_size
-            S = np.mean(elev_array[start_row:(stop_row+1), start_col:end_col])
-            if S < -0.1:
-                elev_array[start_row:(stop_row+1), start_col:end_col] = -0.3
-
-    return elev_array
 
 # load the data as a dataframe
 os.chdir('C:/Users/Lexi/Documents/Research/Outwasher/GIS')
 prestorm_points_df = pd.read_csv("prestorm_points.csv")
 
-# get the column of elevations
+# get the column that contains the elevations
 prestorm_elevations = prestorm_points_df['grid_code']  # same as RawMatrix[,3]
 
-num_cols_domain = 150  # perpendicular side of rectangle
+num_cols_domain = 150  # cross-shore side of rectangle from GIS (barrier width)
 num_rows_domain = int(len(prestorm_elevations)/num_cols_domain)  # this will be the max length of the island
-MHW = 0.36  # meters
+MHW = 0.36  # meters NAVD88? (subtract 0.36 m from NAVD88 elevations to get MHW elevations)
 
 prestorm_elevations = prestorm_elevations - MHW  # convert elevations from NAVD88 to MHW
 
-# plot the entire domain out
+# organizing the list of elevations into an array
 elev_array = np.zeros([num_rows_domain, num_cols_domain])
 start = 0
 end = start + num_cols_domain
@@ -76,42 +35,76 @@ for row in range(num_rows_domain):
     end += num_cols_domain
 
 # set all values less than 0 to -3 m
+# tried range(135) also
 for r in range(135):
     for c in range(num_cols_domain):
         if elev_array[r, c] <= 0:
             elev_array[r, c] = -3
 
-# convert to dam
+# plot the adjusted domain
+# fig1 = plt.figure()
+# ax1 = fig1.add_subplot(111)
+# mat = ax1.matshow(
+#     elev_array,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig1.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax1.set_title("Pre-Storm Elevation")
+# ax1.set_ylabel("barrier width (dam)")
+# ax1.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+
+# convert to dam and make our two segments
 elev_array = elev_array / 10
 elev_array = elev_array[100:-1]
 section1_pre = elev_array[:, 20:70]
 section2_pre = elev_array[:, 70:120]
 
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-mat = ax1.matshow(
-    section1_pre * 10,
-    cmap="terrain",
-    vmin=-3.0,
-    vmax=6.0,
-)
-cbar = fig1.colorbar(mat)
-cbar.set_label('m MHW', rotation=270, labelpad=15)
-ax1.set_title("Pre-Storm Elevation")
-ax1.set_ylabel("barrier width (dam)")
-ax1.set_xlabel("barrier length (dam)")
-plt.gca().xaxis.tick_bottom()
+# # plot section 1
+# fig1 = plt.figure()
+# ax1 = fig1.add_subplot(111)
+# mat = ax1.matshow(
+#     section1_pre * 10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig1.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax1.set_title("Left Pre-Storm Elevation")
+# ax1.set_ylabel("barrier width (dam)")
+# ax1.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+
+# # plot section 2
+# fig1 = plt.figure()
+# ax1 = fig1.add_subplot(111)
+# mat = ax1.matshow(
+#     section2_pre * 10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig1.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax1.set_title("Right Pre-Storm Elevation")
+# ax1.set_ylabel("barrier width (dam)")
+# ax1.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+
 
 ### plotting the post storm
 post_points_df = pd.read_csv("post_storm_points.csv")
 
-# get the column of elevations
+# get the column that contains the elevations
 post_elevations = post_points_df['grid_code']  # same as RawMatrix[,3]
 
 num_cols_domain = 150  # perpendicular side of rectangle
 num_rows_domain = int(len(post_elevations)/num_cols_domain)  # this will be the max length of the island
 post_elevations = post_elevations - MHW  # convert elevations from NAVD88 to MHW
-
 
 # plot the entire domain out
 post_elev_array = np.zeros([num_rows_domain, num_cols_domain])
@@ -128,10 +121,21 @@ for r in range(135):
         if post_elev_array[r, c] <= 0:
             post_elev_array[r, c] = -3
 
-# for r in range(175, num_rows_domain):
-#     for c in range(num_cols_domain):
-#         if post_elev_array[r, c] <= 0:
-#             post_elev_array[r, c] = -3
+# fig2 = plt.figure()
+# ax2 = fig2.add_subplot(111)
+# mat = ax2.matshow(
+#     post_elev_array,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig2.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax2.set_title("Post-Storm Elevation")
+# ax2.set_ylabel("barrier width (dam)")
+# ax2.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+
 
 # convert to dam
 post_elev_array = post_elev_array / 10
@@ -139,32 +143,117 @@ post_elev_array = post_elev_array[100:-1]
 section1_post = post_elev_array[:, 19:69]
 section2_post = post_elev_array[:, 69:119]
 
+# # plot left post section
+# fig2 = plt.figure()
+# ax2 = fig2.add_subplot(111)
+# mat = ax2.matshow(
+#     # post_elev_array[120:-1]*10,
+#     section1_post*10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig2.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax2.set_title("Left Post-Storm Elevation")
+# ax2.set_ylabel("barrier width (dam)")
+# ax2.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+#
+#
+# # plot right post-section
+# fig2 = plt.figure()
+# ax2 = fig2.add_subplot(111)
+# mat = ax2.matshow(
+#     section2_post*10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig2.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax2.set_title("Right Post-Storm Elevation")
+# ax2.set_ylabel("barrier width (dam)")
+# ax2.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+
+# re-formatting the left section
+pre_left = section1_pre[37:78]
+post_left = section1_post[36:77]
+
+difference_left = post_left-pre_left
+total_erosion_left = np.sum(difference_left)*10
+back_erosion_left = np.sum(difference_left[3:30, 14:38])*10
+
+# fig2 = plt.figure()
+# ax2 = fig2.add_subplot(111)
+# mat = ax2.matshow(
+#     pre_left*10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig2.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax2.set_title("Left Zoom Pre-Storm Elevation")
+# ax2.set_ylabel("barrier width (dam)")
+# ax2.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+#
+# fig2 = plt.figure()
+# ax2 = fig2.add_subplot(111)
+# mat = ax2.matshow(
+#     post_left*10,
+#     cmap="terrain",
+#     vmin=-3.0,
+#     vmax=6.0,
+# )
+# cbar = fig2.colorbar(mat)
+# cbar.set_label('m MHW', rotation=270, labelpad=15)
+# ax2.set_title("Left Zoom Post-Storm Elevation")
+# ax2.set_ylabel("barrier width (dam)")
+# ax2.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+#
+# fig3 = plt.figure()
+# ax3 = fig3.add_subplot(111)
+# mat = ax3.matshow(
+#     difference_left*10,
+#     cmap="seismic",
+#     vmin=-5,
+#     vmax=5,
+# )
+# cbar = fig3.colorbar(mat)
+# cbar.set_label('meters', rotation=270, labelpad=15)
+# ax3.set_title("Difference Map")
+# ax3.set_ylabel("barrier width (dam)")
+# ax3.set_xlabel("barrier length (dam)")
+# plt.gca().xaxis.tick_bottom()
+
+
+# break up the domain
+section1_pre_int = pre_left[0:30]
+# section1_pre_int = pre_left[0:32]
+# section1_pre_int[24:, 24:27] = 0.085
+# section1_pre_int[27, 37] = 0.085
+# section1_pre_int[28, 37] = 0.085
+# section1_pre_int[29, 39] = 0.085
+# section1_pre_int[28, 38:40] = 0.085
+# section1_pre_dunes = pre_left[32:35]
+section1_pre_dunes = pre_left[30:35]
+section1_pre_dunes = section1_pre_dunes[1:3]
+# section1_pre_dunes[0:2, 24] = 0.085
+section1_pre_beach = pre_left[35:]
+# section1_pre_beach2 = section1_pre[73:77]
+# section1_pre_beach3 = section1_pre[73:78]
+
+full = np.append(section1_pre_int, section1_pre_dunes, 0)
+full = np.append(full, section1_pre_beach, 0)
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
 mat = ax2.matshow(
-    # post_elev_array[120:-1]*10,
-    section1_post*10,
-    cmap="terrain",
-    vmin=-3.0,
-    vmax=6.0,
-)
-cbar = fig2.colorbar(mat)
-cbar.set_label('m MHW', rotation=270, labelpad=15)
-ax2.set_title("Post-Storm Elevation")
-ax2.set_ylabel("barrier width (dam)")
-ax2.set_xlabel("barrier length (dam)")
-plt.gca().xaxis.tick_bottom()
-
-pre = section1_pre[37:76]
-post = section1_post[36:75]
-difference = post-pre
-total_erosion = np.sum(difference)
-
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-mat = ax2.matshow(
-    pre*10,
+    full*10,
     cmap="terrain",
     vmin=-3.0,
     vmax=6.0,
@@ -176,17 +265,52 @@ ax2.set_ylabel("barrier width (dam)")
 ax2.set_xlabel("barrier length (dam)")
 plt.gca().xaxis.tick_bottom()
 
+
+interior_b3d_input = np.flip(section1_pre_int)
+# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-elevation-left-damMHW-test", interior_b3d_input)
+
+berm_el = 0.11
+dunes_b3d = np.flip(section1_pre_dunes) - berm_el
+dunes_input = np.append(dunes_b3d[0], dunes_b3d[1], 0)
+# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-dunes-left-dam-test", dunes_input)
+
+# the beach should be loaded in with the ocean on the bottom
+# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-beach-left-dam-test", section1_pre_beach)
+
+# re-formatting the right section
+pre_right = section2_pre[36:76]
+post_right = section2_post[35:75]
+
+difference = post_right-pre_right
+total_erosion_right = np.sum(difference)*10
+back_erosion_right = np.sum(difference[4:28, 16:42])*10
+
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
 mat = ax2.matshow(
-    post*10,
+    pre_right*10,
     cmap="terrain",
     vmin=-3.0,
     vmax=6.0,
 )
 cbar = fig2.colorbar(mat)
 cbar.set_label('m MHW', rotation=270, labelpad=15)
-ax2.set_title("Post-Storm Elevation")
+ax2.set_title("Right Edited Pre-Storm Elevation")
+ax2.set_ylabel("barrier width (dam)")
+ax2.set_xlabel("barrier length (dam)")
+plt.gca().xaxis.tick_bottom()
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+mat = ax2.matshow(
+    post_right*10,
+    cmap="terrain",
+    vmin=-3.0,
+    vmax=6.0,
+)
+cbar = fig2.colorbar(mat)
+cbar.set_label('m MHW', rotation=270, labelpad=15)
+ax2.set_title("Right Edited Post-Storm Elevation")
 ax2.set_ylabel("barrier width (dam)")
 ax2.set_xlabel("barrier length (dam)")
 plt.gca().xaxis.tick_bottom()
@@ -206,22 +330,19 @@ ax3.set_ylabel("barrier width (dam)")
 ax3.set_xlabel("barrier length (dam)")
 plt.gca().xaxis.tick_bottom()
 
-## break up the domain
-section1_pre_int = pre[0:30]
-section1_pre_int[24:, 24:27] = 0.085
-section1_pre_int[27, 37] = 0.085
-section1_pre_int[28, 37] = 0.085
-section1_pre_int[29, 39] = 0.085
-section1_pre_int[28, 38:40] = 0.085
-section1_pre_dunes = pre[30:35]
-section1_pre_dunes = section1_pre_dunes[1:3]
-section1_pre_dunes[0:2, 24] = 0.085
-section1_pre_beach = pre[35:]
-section1_pre_beach2 = section1_pre[73:77]
-section1_pre_beach3 = section1_pre[73:78]
+section2_pre_int = pre_right[0:28]
+section2_pre_dunes = pre_right[28:32]
+section2_pre_dunes[0:2, 0:4] += 0.1
+# section2_pre_dunes[0:2, 0:2] = section2_pre_dunes[3, 0:2]
+# section2_pre_dunes[0, 2:5] = section2_pre_dunes[2, 2:5]
+section2_pre_dunes = section2_pre_dunes[0:2]
+section2_pre_dunes[0, 0:20] += 0.05
+section2_pre_dunes[1, 0:16] += 0.1
+# section2_pre_beach = pre[32:]
+section2_pre_beach = np.load(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data\NCB-default-beach.npy")
 
-full = np.append(section1_pre_int, section1_pre_dunes, 0)
-full = np.append(full, section1_pre_beach3, 0)
+full = np.append(section2_pre_int, section2_pre_dunes, 0)
+full = np.append(full, section2_pre_beach, 0)
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
@@ -238,17 +359,13 @@ ax2.set_ylabel("barrier width (dam)")
 ax2.set_xlabel("barrier length (dam)")
 plt.gca().xaxis.tick_bottom()
 
-interior_b3d_input = np.flip(section1_pre_int)
-np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-elevation_mod", interior_b3d_input)
+interior_b3d_input = np.flip(section2_pre_int)
+# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-elevation-right-damMHW-test", interior_b3d_input)
 
-# berm_el = np.mean(section1_pre_beach[0:2])
 berm_el = 0.11
-# max_dunes = np.append(section1_pre_dunes[3, 0:24], section1_pre_dunes[1, 24:])
-# dunes_b3d = np.flip(max_dunes) - berm_el
-dunes_b3d = np.flip(section1_pre_dunes) - berm_el
+dunes_b3d = np.flip(section2_pre_dunes) - berm_el
 dunes_input = np.append(dunes_b3d[0], dunes_b3d[1], 0)
-# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-dunes2", dunes_input)
-#
-# # the beach should be loaded in with the ocean on the bottom
-# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-beach4", section1_pre_beach3)
-# beach_slope = np.mean(section1_pre_beach[0, :] - section1_pre_beach[-1, :])/len(section1_pre_beach)
+# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-dunes-right-dam-test", dunes_input)
+
+# the beach should be loaded in with the ocean on the bottom
+# np.save(r"C:\Users\Lexi\PycharmProjects\CASCADE\cascade\data\outwash_data/NCB-default-beach-right-mod", section2_pre_beach)
