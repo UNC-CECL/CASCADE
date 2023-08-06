3e870686703d656e7cc08b990d531044048bcbf8
Lexi Van Blunk <agvanblu@ncsu.edu>
2023-08-06 12:12:49 -0400

    un-commented some sections of the code

--- scripts/outwash_ms/analyzing_barrier_drowning.py
+++ scripts/outwash_ms/analyzing_barrier_drowning.py
@@ -7,9 +7,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # ---------------------------------- set model parameters that change per run ------------------------------------------
-storm_interval = 20             # 20 or 10 years
-r_dune_growth = 0.25            # 0.25 or 0.35
-config = 4                      # 1, 2, 3, or 4
+storm_interval = 20        # 20 or 10 years
+r_dune_growth = 0.25       # 0.25 or 0.35
+config = 4                 # 1, 2, 3, or 4
 outwash_scenario = "100"   # overwash, 100, 50, 0 (all must be within quotations)
 
 # automatically set min and max r values based on dune growth rate selection
@@ -58,29 +58,29 @@ for storm_num in range(1,101):
     outwash100_obj = outwash100["cascade"][0]
     drowning_array_100[storm_num-1] = outwash100_obj.barrier3d[0].drown_break
 
-    # # 50% variables
-    # filename_50 = "config{0}_outwash50_startyr1_interval{1}yrs_Slope0pt03_{2}.npz".format(config, storm_interval, storm_num)
-    # file_50 = datadir_50 + filename_50
-    # outwash50 = np.load(file_50, allow_pickle=True)
-    # outwash50_obj = outwash50["cascade"][0]
-    # drowning_array_50[storm_num-1] = outwash50_obj.barrier3d[0].drown_break
-    #
-    # # 0% variables
-    # filename_0 = "config{0}_outwash0_startyr1_interval{1}yrs_Slope0pt03_{2}.npz".format(config, storm_interval, storm_num)
-    # file_0 = datadir_0 + filename_0
-    # outwash0 = np.load(file_0, allow_pickle=True)
-    # outwash0_obj = outwash0["cascade"][0]
-    # drowning_array_0[storm_num-1] = outwash0_obj.barrier3d[0].drown_break
+    # 50% variables
+    filename_50 = "config{0}_outwash50_startyr1_interval{1}yrs_Slope0pt03_{2}.npz".format(config, storm_interval, storm_num)
+    file_50 = datadir_50 + filename_50
+    outwash50 = np.load(file_50, allow_pickle=True)
+    outwash50_obj = outwash50["cascade"][0]
+    drowning_array_50[storm_num-1] = outwash50_obj.barrier3d[0].drown_break
+
+    # 0% variables
+    filename_0 = "config{0}_outwash0_startyr1_interval{1}yrs_Slope0pt03_{2}.npz".format(config, storm_interval, storm_num)
+    file_0 = datadir_0 + filename_0
+    outwash0 = np.load(file_0, allow_pickle=True)
+    outwash0_obj = outwash0["cascade"][0]
+    drowning_array_0[storm_num-1] = outwash0_obj.barrier3d[0].drown_break
 
 # because we have 100 storms and drowns = 1, the sum of the array is the percent that drowns
 percent_drown_b3d = np.sum(drowning_array_b3d)
-print("{0} percent of barriers drown for the overwash only scenario".format(percent_drown_b3d))
+print("{0}% of barriers drown for the overwash only scenario".format(int(percent_drown_b3d)))
 
 percent_drown_100 = np.sum(drowning_array_100)
-print("{0} percent of barriers drown for the 100% outwash to shoreface scenario".format(percent_drown_100))
+print("{0}% of barriers drown for the 100% outwash to shoreface scenario".format(int(percent_drown_100)))
 
-# percent_drown_50 = np.sum(drowning_array_50)
-# print("{0} percent of barriers drown for the 50% outwash to shoreface scenario".format(percent_drown_50))
-#
-# percent_drown_0 = np.sum(drowning_array_0)
-# print("{0} percent of barriers drown for the 0% outwash to shoreface scenario".format(percent_drown_0))
+percent_drown_50 = np.sum(drowning_array_50)
+print("{0}% of barriers drown for the 50% outwash to shoreface scenario".format(int(percent_drown_50)))
+
+percent_drown_0 = np.sum(drowning_array_0)
+print("{0}% of barriers drown for the 0% outwash to shoreface scenario".format(int(percent_drown_0)))
