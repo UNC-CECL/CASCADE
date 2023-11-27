76d7afb8450d6e31dee276bb942c1da1a9978c1c
Benton Franklin <85505371+franklin1895@users.noreply.github.com>
2023-11-27 10:29:46 -0500

    New 5% storm values

--- scripts/marsh_ms/Generate_Storms.py
+++ scripts/marsh_ms/Generate_Storms.py
@@ -16,8 +16,9 @@ RMAX = storm_list[:,2]
 Mean_Rmax = np.mean(RMAX)
 Mean_Rmax_10 = Mean_Rmax*.1
 Mean_Rmax_02 = Mean_Rmax*.02
+Mean_Rmax_05 = Mean_Rmax*.05
 
-
+'''
 sys.path
 for i in range(0,50):
     base_storm_series = input_files.shift_storm_intensity(
@@ -69,3 +70,20 @@ for i in range(0,50):
         output_filename="StormList_10kyrs_VCR_Berm1pt9m_Slope0pt04-lowIntensity",
     )
     np.save(file='/Users/ceclmac/OneDrive - University of North Carolina at Chapel Hill/Chapter 2/Storms/Two_Percent_Increase/StormList_'+str(i)+'_2_percent_increase.npy',arr=storm_series_shifted_02)
+'''
+for i in range(0,50):
+    storm_series_shifted_05 = input_files.shift_storm_intensity(
+        datadir=data_path,
+        storm_list_name="StormList_20k_VCR_Berm1pt9m_Slope0pt04.csv",  # can by .py or .csv
+        mean_yearly_storms=8.3,
+        SD_yearly_storms=5.9,
+        shift=Mean_Rmax_05,  # shift the TWL distribution to the right (i.e., increase intensity), m NAVD88, typically [-0.15, 0.15]
+        MHW=0.46,  # m NAVD88
+        StormStart=2,
+        BermEl=1.9,  # m NAVD88, just used for plotting
+        model_years=1000,
+        bPlot=False,
+        bSave=False,
+        output_filename="StormList_10kyrs_VCR_Berm1pt9m_Slope0pt04-lowIntensity",
+    )
+    np.save(file='/Users/ceclmac/OneDrive - University of North Carolina at Chapel Hill/Chapter 2/Storms/Five_Percent_Increase/StormList_'+str(i)+'_5_percent_increase.npy',arr=storm_series_shifted_05)
