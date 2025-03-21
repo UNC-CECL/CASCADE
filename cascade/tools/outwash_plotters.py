41d3ef73815c79e47b1515b3a37f99e955aa9bf0
Lexi Van Blunk <agvanblu@ncsu.edu>
2025-03-21 19:53:35 -0400

    added a figure for making the subplot figure of CASCADE elevations. similar to the animate elevations, but organizes specific years into a subplot and saves one final plot

--- cascade/tools/outwash_plotters.py
+++ cascade/tools/outwash_plotters.py
@@ -524,3 +524,278 @@ def plot_ElevAnimation_CASCADE(
     # imageio.mimsave("elev.gif", frames, "GIF-FI")
     print()
     print("[ * GIF successfully generated * ]")
+
+
+def plot_Elev_CASCADE_subplots(
+    cascade,
+    directory,
+    TMAX_MGMT,
+    name,
+    TMAX_SIM,
+    plot_timesteps,  # array with the timesteps you want to plot
+    main_plot_title,  # title of the main plot (string)
+    ny=1,
+    beach_management_ny=None,  # list of bool the length of ny, or None for all False
+    roadway_management_ny=None,
+    y_lim=(150, 250),
+    z_lim=6,
+    z_bot=-3,
+    fig_size=None,
+    fig_eps=False,
+    km_on=False,
+    meters_on=True,
+):
+    """
+    :param cascade: a cascade model object
+    :param directory: for saving (string)
+    :param TMAX_MGMT: last time index that the b3d subgrid was managed (list)
+    :param name: for saving (string)
+    :param TMAX_SIM: last time index that the b3d subgrid was managed (list)
+    :param ny: the number of barrier3d subgrids that you want to plot
+    :param beach_management_ny: booleans for b3d subgrids that used
+        BeachDuneManager (list)
+    :param roadway_management_ny: booleans for b3d subgrids that used
+        RoadwayManager (list)
+    :param y_lim: y limits [low, high] of plot [dam]
+    :param z_lim: z limit of plot [m MHW]
+    :param fig_size: size of plot, e.g., (6, 2.5)
+    :param fig_eps: output files in eps format -- default is png [best for gif]
+    :return: gif
+
+    Animation Frames of Barrier and Dune Elevation (#4 in Barrier3D_Functions,
+    modified here for CASCADE)
+
+    NOTE THAT THE BEACH REPRESENTATION IS BASED ON A MODEL SPECIFIED BEACH WIDTH.
+    We set the beach width for the remaining time steps after the community has
+    been abandoned to the last managed beach width in order to not have a huge
+    jump in the back-barrier position in Barrier3D. OTHERWISE, it is meaningless
+    to the dynamics Barrier3D.
+
+    Updated so that beach width represented here is the outwasher beach width
+    """
+    if beach_management_ny is None:
+        beach_management_ny = [False] * ny
+    if roadway_management_ny is None:
+        roadway_management_ny = [False] * ny
+
+    barrier3d = cascade.barrier3d
+    outwash = cascade.outwash
+
+    # set up the domain; here we just use the first grid, but that could break in
+    # future runs
+    BarrierLength = barrier3d[0].BarrierLength
+    if np.any(beach_management_ny):
+        indices = [i for i in range(ny) if beach_management_ny[i] == 1]
+        iB3D = indices[0]
+        MaxBeachWidth = (
+            np.max(cascade.nourishments[iB3D].beach_width[0 : TMAX_MGMT[iB3D]]) / 10
+        )  # dam
+    else:
+        MaxBeachWidth = cascade._initial_beach_width[0]
+    OriginY = int(barrier3d[0].x_s_TS[0])
+    AniDomainWidth = int(
+        np.amax(barrier3d[0].InteriorWidth_AvgTS)
+        + MaxBeachWidth
+        + np.abs(barrier3d[0]._ShorelineChange)
+        + OriginY
+        + 35
+    )
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
+    newpath = name
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    os.chdir(newpath)
+
+    maxMGMT = np.max(TMAX_MGMT)
+    plt.rcParams.update({"font.size": 10})
+
+    # establish the whole figure (not subplots yet)
+    plot_num = 1
+    n_cols = len(plot_timesteps)
+    if fig_size is not None:
+        elevFig2 = plt.figure(figsize=fig_size)
+    else:
+        elevFig2 = plt.figure(figsize=(25, 3))
+    elevFig2.suptitle('{0}'.format(main_plot_title), weight="bold", fontsize=12)
+
+    if km_on:
+        # elevFig2.supxlabel("alongshore distance (km)")
+        # elevFig2.supylabel("cross-shore distance (km)")
+        x_text = "alongshore distance (km)"
+        y_text = "cross-shore distance (km)"
+    if meters_on:
+        x_text = "alongshore distance (m)"
+        y_text = "cross-shore distance (m)"
+    else:
+        x_text = "alongshore distance (dam)"
+        y_text = "cross-shore distance (dam)"
+
+    elevFig2.text(0.02, 0.5, y_text, fontsize=10, rotation="vertical", ha="center", va="center")
+    elevFig2.text(0.51, 0.05, x_text, fontsize=10, rotation="horizontal", ha="center", va="center")
+
+    # loop to make each domain and plot it as a subplot
+    for t in plot_timesteps:
+        # ok, now the annual time step, which incorporates human modifications to
+        # the shoreface, beach, dune, & interior
+        AnimateDomain = np.ones([AniDomainWidth + 1, BarrierLength * ny]) * -3
+
+        if t <= TMAX_SIM:
+
+            for iB3D in range(ny):
+                actual_shoreline_post_humans = barrier3d[iB3D].x_s_TS[0 : TMAX_SIM + 1]
+
+                if beach_management_ny[iB3D]:
+                    beach_width = cascade.nourishments[iB3D].beach_width[t] / 10
+                else:
+                    # both roadways and natural scenario
+                    # beach_width = cascade._initial_beach_width[iB3D] / 10
+
+                    # CHANGED FOR THE OUTWASHER BEACH RATHER THAN THE CASCADE BEACH
+                    beach_width = outwash[iB3D]._beach_width[t] / 10
+                    if np.isnan(beach_width):
+                        beach_width = 0
+
+                if beach_management_ny[iB3D] and np.isnan(beach_width):
+                    beach_width = (
+                        cascade.nourishments[iB3D].beach_width[t - 1] / 10
+                    )  # this can happen if the barrier height drowns
+
+                cellular_dune_toe_post_humans = np.floor(
+                    actual_shoreline_post_humans[t] + beach_width
+                )
+
+                cellular_shoreline_post_humans = np.floor(actual_shoreline_post_humans[t])
+                cellular_beach_width = int(
+                    cellular_dune_toe_post_humans - cellular_shoreline_post_humans
+                )  # not actual bw
+
+                if t == maxMGMT + 1:
+                    cellular_beach_width_final = (
+                        cellular_beach_width  # save this for plotting moving forward
+                    )
+                if t > maxMGMT + 1:
+                    cellular_beach_width = cellular_beach_width_final
+
+                BeachDomain = np.zeros(
+                    [
+                        cellular_beach_width,
+                        BarrierLength,
+                    ]
+                )
+
+                if cellular_beach_width == 0:
+                    pass
+                else:
+                    add = (barrier3d[iB3D].BermEl - barrier3d[iB3D].SL) / (
+                        cellular_beach_width + 1
+                    )
+                    for i in range(0, cellular_beach_width):
+                        BeachDomain[i, :] = (barrier3d[iB3D].SL + add) * (i + 1)
+
+                # Make animation frame domain
+                # so this is now in meters?
+                Domain = barrier3d[iB3D].DomainTS[t] * 10
+                Dunes = (barrier3d[iB3D].DuneDomain[t, :, :] + barrier3d[iB3D].BermEl) * 10
+                Dunes = np.rot90(Dunes)
+                Dunes = np.flipud(Dunes)
+                Beach = BeachDomain * 10
+                Domain = np.flip(np.vstack([Beach, Dunes, Domain]),1)
+                Domain[Domain < 0] = -3
+                widthTS = len(Domain)
+                OriginTstart = int(cellular_shoreline_post_humans)
+                OriginTstop = OriginTstart + widthTS
+                xOrigin = iB3D * BarrierLength
+                AnimateDomain[
+                    OriginTstart:OriginTstop, xOrigin : xOrigin + BarrierLength
+                ] = Domain
+
+        # Plot to subplot and save final figure
+        plt.rcParams.update({"font.size": 10})
+
+        ax = elevFig2.add_subplot(1, n_cols, plot_num)
+        cax = ax.pcolormesh(
+            AnimateDomain,
+            cmap="terrain",
+            vmin=z_bot,
+            vmax=z_lim,
+            # edgecolors="w",  # for debugging
+            # linewidth=0.01,
+        )
+
+        # we only want the color bar for the last plot
+        # we also want no y-axis labels or ticks, but we do want x-axis ones
+        if plot_num == len(plot_timesteps):
+            cbar = elevFig2.colorbar(cax)
+            cbar.set_label("elevation (m MHW)", rotation=270, labelpad=10)
+            if km_on:
+                locs, _ = plt.xticks()
+                plt.xticks(locs[0:-1], locs[0:-1] / 100)
+            if meters_on:
+                xtick_max = np.shape(AnimateDomain)[1]  # n_cols = x
+                x_ticks = np.array(range(0, xtick_max, 25))
+                x_tick_labels = x_ticks * 10
+                plt.xticks(x_ticks, x_tick_labels)
+                ax.set_yticks([])
+
+        # we only want the cross-shore (y-axis) ticks for the first label, not the rest
+        # but we also want xticks for this lable
+        elif plot_num == 1:
+            if km_on:
+                locs, _ = plt.yticks()
+                plt.yticks(locs, locs / 100)
+                locs, _ = plt.xticks()
+                plt.xticks(locs[0:-1], locs[0:-1] / 100)
+                ax.yaxis.set_minor_locator(AutoMinorLocator())
+            if meters_on:
+                xtick_max = np.shape(AnimateDomain)[1]  # n_cols = x
+                x_ticks = np.array(range(0, xtick_max, 25))
+                x_tick_labels = x_ticks * 10
+                ytick_max = np.shape(AnimateDomain)[0]  # n_rows = y
+                y_ticks = np.array(range(0, ytick_max, 25))
+                y_tick_labels = y_ticks * 10
+                plt.xticks(x_ticks, x_tick_labels)
+                plt.yticks(y_ticks, y_tick_labels)
+        # we want the x-axis ticks for all the labels
+        else:
+            if km_on:
+                # locs, _ = plt.yticks()
+                # plt.yticks(locs, locs / 100)
+                locs, _ = plt.xticks()
+                plt.xticks(locs[0:-1], locs[0:-1] / 100)
+                # ax.yaxis.set_minor_locator(AutoMinorLocator())
+            if meters_on:
+                xtick_max = np.shape(AnimateDomain)[1]  # n_cols = x
+                x_ticks = np.array(range(0, xtick_max, 25))
+                x_tick_labels = x_ticks * 10
+                plt.xticks(x_ticks, x_tick_labels)
+                ax.set_yticks([])
+
+        timestr = "Year " + str(t)
+        ax.set_title(timestr,fontsize=10)
+        if y_lim is not None:
+            plt.ylim(y_lim)
+            # plt.text(3, y_lim[0] + 3, timestr, color="w")
+        else:
+            plt.ylim(bottom=OriginY - 35)
+            # plt.text(1, OriginY - 33, timestr)
+        # plt.tight_layout()
+        # plt.rcParams.update({"font.size": 10})
+        plot_num += 1
+
+    plt.subplots_adjust(top=0.8, bottom=0.2, left=0.06, right=0.95)
+    # plt.subplots_adjust(left=0.05, bottom=0.1, right=0.06, top=0.15, wspace=-0.5)
+
+    # once we make the entire plot, save it
+    if fig_eps:
+        figname = "elev_" + name + ".eps"
+        elevFig2.savefig(figname, format="eps")
+    else:
+        figname = "elev_" + name
+        elevFig2.savefig(figname)  # dpi=200
+    plt.close(elevFig2)
+
+    print()
+    print("[ * figure successfully generated * ]")
