e0906e7a20317b3be403a168f1dda85d74835cc6
Lexi Van Blunk <agvanblu@ncsu.edu>
2026-03-06 14:10:53 -0500

    changed the name convention for easier interpretation and deleted unnecessary content

--- scripts/outwash_ms/creating_outwash_storms.py
+++ scripts/outwash_ms/creating_outwash_storms.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 # ---------------------- load the ADCIRC-generated storm data (text file) from Chris -----------------------------------
 
-datadir = "C:/Users/Lexi/PycharmProjects/CASCADE/data/outwash_data/"
+datadir = "C:/Users/Lexi/PycharmProjects/CASCADE/data/init_outwasher/"
 file_name = "sound_data.txt"  # the ADCIRC generated hydrograph from USGS
 hydro_mMSL = np.loadtxt(datadir + file_name, delimiter=",")
 # use Beaufort, Duke Marine Lab NC datums to convert to dam MHW: MHW is 0.47 m MSL, dam = m / 10
@@ -94,65 +94,11 @@ if save_hydro:
     name_hydro = "outwash_storms_6min.npy"
     np.save(datadir + name_hydro, hydro_damMHW_freq_increase)
 
-# all for plotting the incrememnts on a 12-hr x axis (I think)
-# new_ss_list = []
-# for s in range(len(new_ss)):
-#     if s == 0:
-#         new_ss_list.append(new_ss[s])
-#     elif s % 8 == 0:
-#         new_ss_list.append(new_ss[s])
-#
-# new_ss = np.asarray(new_ss_list)
-
-# plt.figure(3)
-# x_3 = np.asarray(range(len(new_ss)))*8
-# x_1 = []
-#
-# for t in x_3:
-#     if t == 0:
-#         x_1.append(t)
-#     elif t % 60 == 0:
-#         x_1.append(t)
-#
-# plt.plot(x_3, new_ss*10, color='darkgrey')
-# plt.scatter(x_3, new_ss*10, color="b")
-# plt.scatter(x_1, sound_levels*10, color="r")
-# plt.xlabel("hours")
-# plt.ylabel("m MHW")
-# label_locs = np.arange(0, max(x_3)+1, step=120, dtype=int)
-# labels = label_locs // 60
-# plt.xticks(label_locs, labels)
-#
-# plt.title("8 Minute Hydrograph")
-
-# plt.figure(3)
-# x_15 = np.asarray(range(len(new_ss)))*15
-# x_1 = []
-#
-# for t in x_15:
-#     if t == 0:
-#         x_1.append(t)
-#     elif t % 60 == 0:
-#         x_1.append(t)
-#
-# plt.plot(x_15, new_ss*10, color='darkgrey')
-# plt.scatter(x_15, new_ss*10, color="b")
-# plt.scatter(x_1, sound_levels*10, color="r")
-# plt.xlabel("hours")
-# plt.ylabel("m MHW")
-# label_locs = np.arange(0, max(x_15)+1, step=120, dtype=int)
-# labels = label_locs // 60
-# plt.xticks(label_locs, labels)
-#
-# plt.title("15 Minute Hydrograph")
-
-# creating the full storm series (years in column 1 and hydrograph in column 2)
-
 
 # ------------------------------ create the storm series (specifying outwash years) ------------------------------------
 
 num_simulation_years = 100  # total CASCADE model simulation years
-num_storms = 1  # the number of outwash storms to occur over the total CASCADE model simulation years
+num_storms = 5  # the number of outwash storms to occur over the total CASCADE model simulation years
 outwash_storms = np.empty(
     [num_storms, 2], dtype=object
 )  # has to be object bc we are adding an array to the second col
@@ -173,5 +119,5 @@ for storm in range(num_storms):
 # option to save the storm series
 save_storm_series = False
 if save_storm_series:
-    name = "outwash_storms_yr1_only"
+    name = "outwash_storms_{0}yr_interval".format(interval)
     np.save(datadir + name, outwash_storms)
