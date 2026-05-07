2d356bcbad4b030d25ee29ce8b1fb527a192a820
Lexi Van Blunk <agvanblu@ncsu.edu>
2026-05-07 09:22:24 -0400

    added the dune domain and units options

--- cascade/tools/plan_view_plotter.py
+++ cascade/tools/plan_view_plotter.py
@@ -9,7 +9,7 @@ import imageio
 import math
 
 
-def plot_domains_dam(casc_model, save_dir, newfolder, buffer):
+def plot_domains_dam(casc_model, save_dir, newfolder, buffer, units):
     """
 
     :param casc_model: string
@@ -19,11 +19,13 @@ def plot_domains_dam(casc_model, save_dir, newfolder, buffer):
     :param newfolder: string
         new folder name (figures will go in this folder which will be located in the save_dir)
     :param buffer: int [dam]
-        extra space at top (marsh-side) of the cascade plots so the barrier is surrounded by water
+        extra space on the oceanside of the cascade plots so the barrier is surrounded by water
+    :param units: string
+        "m" or "km" to change the axes units
     :return:
     """
     for t in range(int(casc_model.TMAX)-1):
-    # for t in range(3):
+    # for t in range(1):
 
         # determine the necessary width of the figure (maximum of the shoreline position + barrier width at each TS)
         total_width_list = []
@@ -38,10 +40,21 @@ def plot_domains_dam(casc_model, save_dir, newfolder, buffer):
         # initialize figure using bay depth of -0.3 dam
         domain = np.ones([plot_width, casc_model.BarrierLength]) * -0.3  # [dam]
         start_width = math.floor(casc_model.x_s_TS[t] / 10)  # [dam] rounded down to nearest integer
-        domain_width = np.shape(casc_model.DomainTS[t])[0]  # [dam]
-        end_width = start_width + domain_width  # [dam]
-        casc_model.DomainTS[t][casc_model.DomainTS[t] <=0 ] = -0.3  # set all values < 0 to bay depth
-        domain[start_width:end_width, :] = casc_model.DomainTS[t]
+
+        # add the barrier dune domain
+        dunes = (casc_model.DuneDomain[t] + casc_model.BermEl)  # [dam]
+        dunes = np.rot90(dunes)   # left column (oceanside) is now the bottom row
+        dunes = np.flipud(dunes)  # switch rows so oceanside is now on top to match orientation of the interior
+
+        # add the barrier interior domain - oriented high elevation (ocean) on top, marsh on bottom
+        int_domain = casc_model.DomainTS[t]
+        # int_domain[int_domain <= 0] = -0.3  # set all values <= 0 to water
+        full_domain = np.vstack([dunes, int_domain])
+
+        # set the barrier domain in the water domain
+        domain_width = np.shape(full_domain)[0]  # [dam]
+        end_width = start_width + domain_width   # [dam]
+        domain[start_width:end_width, :] = full_domain
 
         # plotting the domains for comparison
         plt.rcParams.update({"font.size": 11})
@@ -50,23 +63,46 @@ def plot_domains_dam(casc_model, save_dir, newfolder, buffer):
         elevFig1 = plt.figure(figsize=(10, 8))
         ax1 = elevFig1.add_subplot(111)
         cax1 = ax1.pcolormesh(
-            domain * 10,
+            domain * 10,  # [m]
             cmap="terrain",
-            vmin=-3,
+            vmin=-1,
             vmax=6,
             # edgecolors="w",  # for debugging
             # linewidth=0.01,
         )
         cbar1 = elevFig1.colorbar(cax1)
         cbar1.set_label("elevation (m MHW)", rotation=270)
-        ax1.set_xlabel("alongshore distance (dam)")
-        ax1.set_ylabel("cross-shore distance (dam)")
-        # timestr = (
-        #         "Time = " + str(t) + " yrs"
-        # )
-        # ax1.text(0, 0, timestr)
+
+        # add text and set title
+        ax1.text(0.5, 0.01, 'Ocean', c="white", horizontalalignment='center',
+         verticalalignment='center', transform=ax1.transAxes)
+        ax1.text(0.5, 0.98, 'Marsh/Bay', c="white", horizontalalignment='center',
+             verticalalignment='center', transform=ax1.transAxes)
         ax1.set_title("Year {0}".format(t))
-        ax1.set_ylim([0, plot_width])
+
+        # set axis labels
+        if units == "km":
+            xticks = ax1.get_xticks()
+            xlabels = xticks / 100
+            ax1.set_xticklabels(xlabels)
+            yticks = ax1.get_yticks()
+            ylabels = yticks / 100
+            ax1.set_yticklabels(ylabels)
+            ax1.set_xlabel("alongshore distance (km)")
+            ax1.set_ylabel("cross-shore distance (km)")
+        elif units == "m":
+            xticks = ax1.get_xticks()
+            xlabels = xticks * 10
+            ax1.set_xticklabels(xlabels.astype(int))
+            yticks = ax1.get_yticks()
+            ylabels = yticks * 10
+            ax1.set_yticklabels(ylabels.astype(int))
+            ax1.set_xlabel("alongshore distance (m)")
+            ax1.set_ylabel("cross-shore distance (m)")
+        else:
+            ax1.set_xlabel("alongshore distance (dam)")
+            ax1.set_ylabel("cross-shore distance (dam)")
+
 
         # Save
         newpath = os.path.join(save_dir, newfolder)
