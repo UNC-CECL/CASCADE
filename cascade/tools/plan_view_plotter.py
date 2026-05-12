daaf869c565729eeb7200909f21ff8a92e247a7c
Lexi Van Blunk <agvanblu@ncsu.edu>
2026-05-12 09:03:37 -0400

    in the transect plotter, converted transect to MHW so it is consistent with the domain plot changed plot limits suppressed the plot pop-ups formatting changes

--- cascade/tools/plan_view_plotter.py
+++ cascade/tools/plan_view_plotter.py
@@ -7,6 +7,9 @@ import os
 import matplotlib.pyplot as plt
 import imageio
 import math
+import warnings
+
+warnings.simplefilter("ignore", category=UserWarning)
 
 
 def plot_domains(casc_model, iB3D, save_dir, newfolder, buffer, units):
@@ -24,8 +27,10 @@ def plot_domains(casc_model, iB3D, save_dir, newfolder, buffer, units):
         "m" or "km" to change the axes units
     :return:
     """
-    for t in range(int(casc_model.barrier3d[iB3D].TMAX)-1):
-    # for t in range(1):
+    for t in range(int(casc_model.barrier3d[iB3D].TMAX) - 1):
+        # for t in range(1):
+
+        plt.ioff()  # do not open windows when creating the plots
 
         # determine the necessary width of the figure (maximum of the shoreline position + barrier width at each TS)
         total_width_list = []
@@ -43,7 +48,7 @@ def plot_domains(casc_model, iB3D, save_dir, newfolder, buffer, units):
 
         # add the barrier dune domain
         dunes = (casc_model.barrier3d[iB3D].DuneDomain[t] + casc_model.barrier3d[iB3D].BermEl)  # [dam]
-        dunes = np.rot90(dunes)   # left column (oceanside) is now the bottom row
+        dunes = np.rot90(dunes)  # left column (oceanside) is now the bottom row
         dunes = np.flipud(dunes)  # switch rows so oceanside is now on top to match orientation of the interior
 
         # add the barrier interior domain - oriented high elevation (ocean) on top, marsh on bottom
@@ -53,7 +58,7 @@ def plot_domains(casc_model, iB3D, save_dir, newfolder, buffer, units):
 
         # set the barrier domain in the water domain
         domain_width = np.shape(full_domain)[0]  # [dam]
-        end_width = start_width + domain_width   # [dam]
+        end_width = start_width + domain_width  # [dam]
         domain[start_width:end_width, :] = full_domain
 
         # plotting the domains for comparison
@@ -67,15 +72,17 @@ def plot_domains(casc_model, iB3D, save_dir, newfolder, buffer, units):
             cmap="terrain",
             vmin=-1,
             vmax=6,
-        )
+            )
         cbar1 = elevFig1.colorbar(cax1)
         cbar1.set_label("elevation (m MHW)", rotation=270)
 
         # add text and set title
-        ax1.text(0.5, 0.01, 'Ocean', c="white", horizontalalignment='center',
-         verticalalignment='bottom', transform=ax1.transAxes)
-        ax1.text(0.5, 0.98, 'Marsh/Bay', c="white", horizontalalignment='center',
-             verticalalignment='top', transform=ax1.transAxes)
+        ax1.text(
+            0.5, 0.01, 'Ocean', c="white", horizontalalignment='center',
+            verticalalignment='bottom', transform=ax1.transAxes)
+        ax1.text(
+            0.5, 0.98, 'Marsh/Bay', c="white", horizontalalignment='center',
+            verticalalignment='top', transform=ax1.transAxes)
         ax1.set_title("Year {0}".format(t))
 
         # set axis labels
@@ -101,7 +108,6 @@ def plot_domains(casc_model, iB3D, save_dir, newfolder, buffer, units):
             ax1.set_xlabel("alongshore distance (dam)")
             ax1.set_ylabel("cross-shore distance (dam)")
 
-
         # Save
         newpath = os.path.join(save_dir, newfolder)
         if not os.path.exists(newpath):
@@ -114,12 +120,13 @@ def plot_domains(casc_model, iB3D, save_dir, newfolder, buffer, units):
 
 def create_gif(casc_model, iB3D, folder_path):
     frames = []
-    for filenum in range(int(casc_model.barrier3d[iB3D].TMAX)-1):
+    for filenum in range(int(casc_model.barrier3d[iB3D].TMAX) - 1):
         filename = os.path.join(folder_path, "DomainTS_{0}.png".format(filenum))
         frames.append(imageio.imread(filename))
     imageio.mimsave(os.path.join(folder_path, "DomainTS.gif"), frames, "GIF-FI", fps=5)
     print()
     print("[ * GIF successfully generated * ]")
+
 
 def plot_domains_with_marsh_transect(casc_model, iB3D, save_dir, newfolder, buffer, units):
     """
@@ -136,8 +143,10 @@ def plot_domains_with_marsh_transect(casc_model, iB3D, save_dir, newfolder, buff
         "m" or "km" to change the axes units
     :return:
     """
-    for t in range(int(casc_model.barrier3d[iB3D].TMAX)-1):
-    # for t in range(20):
+    for t in range(int(casc_model.barrier3d[iB3D].TMAX) - 1):
+    # for t in range(5):
+
+        plt.ioff()  # do not open windows when creating the plots
 
         # determine the necessary width of the figure (maximum of the shoreline position + barrier width at each TS)
         total_width_list = []
@@ -155,7 +164,7 @@ def plot_domains_with_marsh_transect(casc_model, iB3D, save_dir, newfolder, buff
 
         # add the barrier dune domain
         dunes = (casc_model.barrier3d[iB3D].DuneDomain[t] + casc_model.barrier3d[iB3D].BermEl)  # [dam]
-        dunes = np.rot90(dunes)   # left column (oceanside) is now the bottom row
+        dunes = np.rot90(dunes)  # left column (oceanside) is now the bottom row
         dunes = np.flipud(dunes)  # switch rows so oceanside is now on top to match orientation of the interior
 
         # add the barrier interior domain - oriented high elevation (ocean) on top, marsh on bottom
@@ -165,23 +174,54 @@ def plot_domains_with_marsh_transect(casc_model, iB3D, save_dir, newfolder, buff
 
         # set the barrier domain in the water domain
         domain_width = np.shape(full_domain)[0]  # [dam]
-        end_width = start_width + domain_width   # [dam]
+        end_width = start_width + domain_width  # [dam]
         domain[start_width:end_width, :] = full_domain
 
         # add the transect
-        BB_transect_m1 = np.flip(casc_model.barrierbmft[iB3D].bmftc_BB.elevation[casc_model.barrierbmft[iB3D].bmftc_BB.startyear + t - 1, int(casc_model.barrierbmft[iB3D].bmftc_BB.Marsh_edge[casc_model.barrierbmft[iB3D].bmftc_BB.startyear + t]):])
+        BB_transect_m1 = np.flip(
+            casc_model.barrierbmft[iB3D].bmftc_BB.elevation[casc_model.barrierbmft[iB3D].bmftc_BB.startyear + t - 1,
+            int(
+                casc_model.barrierbmft[iB3D].bmftc_BB.Marsh_edge[
+                    casc_model.barrierbmft[iB3D].bmftc_BB.startyear + t]):])
         if casc_model.barrierbmft[iB3D].x_b_TS_ML[t] < 0:
-            ML_transect_m1 = np.append(np.ones([abs(int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]))]) * casc_model.barrierbmft[iB3D].bmftc_ML.elevation[casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t - 1, 1], casc_model.barrierbmft[iB3D].bmftc_ML.elevation[casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t - 1, :])
+            ML_transect_m1 = np.append(
+                np.ones([abs(int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]))]) *
+                casc_model.barrierbmft[iB3D].bmftc_ML.elevation[
+                    casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t - 1, 1],
+                casc_model.barrierbmft[iB3D].bmftc_ML.elevation[casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t - 1,
+                :])
         elif casc_model.barrierbmft[iB3D].x_b_TS_ML[t] > 0:
-            ML_transect_m1 = casc_model.barrierbmft[iB3D].bmftc_ML.elevation[casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t - 1, int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]):]
+            ML_transect_m1 = casc_model.barrierbmft[iB3D].bmftc_ML.elevation[
+                             casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t - 1,
+                             int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]):]
 
         whole_transect_m1 = np.append(BB_transect_m1, ML_transect_m1)
 
+        # convert to MHW
+        conversion = (casc_model.barrierbmft[iB3D].bmftc_BB.msl[
+                                                         casc_model.barrierbmft[iB3D].bmftc_BB.startyear + t - 1] +
+                                                     casc_model.barrierbmft[
+                                                         iB3D].bmftc_BB.amp)  # Make marsh elevation relative to MHW datum
+        whole_transect_mhw = whole_transect_m1 - conversion
+
         # Extract marsh & forest locations to plot
-        x_forest_m1 = [casc_model.barrierbmft[iB3D].bmftc_BB.B - int(casc_model.barrierbmft[iB3D].bmftc_BB.Forest_edge[casc_model.barrierbmft[iB3D].bmftc_BB.startyear + t]), len(BB_transect_m1) - int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]) + int(casc_model.barrierbmft[iB3D].bmftc_ML.Forest_edge[casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t])]
-        x_marsh_m1 = [len(BB_transect_m1) - 1, len(BB_transect_m1) - int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]) + int(casc_model.barrierbmft[iB3D].bmftc_ML.Marsh_edge[casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t])]
+        x_forest_m1 = [casc_model.barrierbmft[iB3D].bmftc_BB.B - int(
+            casc_model.barrierbmft[iB3D].bmftc_BB.Forest_edge[casc_model.barrierbmft[iB3D].bmftc_BB.startyear + t]),
+                       len(BB_transect_m1) - int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]) + int(
+                           casc_model.barrierbmft[iB3D].bmftc_ML.Forest_edge[
+                               casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t])]
+        x_marsh_m1 = [len(BB_transect_m1) - 1,
+                      len(BB_transect_m1) - int(casc_model.barrierbmft[iB3D].x_b_TS_ML[t]) + int(
+                          casc_model.barrierbmft[iB3D].bmftc_ML.Marsh_edge[
+                              casc_model.barrierbmft[iB3D].bmftc_ML.startyear + t])]
         y_forest_m1 = whole_transect_m1[x_forest_m1]
         y_marsh_m1 = whole_transect_m1[x_marsh_m1]
+
+        # convert to MHW
+        # x-position has not changed, just the value of y
+        y_forest_mhw = whole_transect_mhw[x_forest_m1]
+        y_marsh_mhw = whole_transect_mhw[x_marsh_m1]
+
 
         # plot
         plt.rcParams.update({"font.size": 11})
@@ -197,16 +237,18 @@ def plot_domains_with_marsh_transect(casc_model, iB3D, save_dir, newfolder, buff
             domain * 10,  # [m]
             cmap="terrain",
             vmin=-1,
-            vmax=6,
-        )
+            vmax=3,
+            )
         cbar1 = elevFig1.colorbar(cax1)
         cbar1.set_label("elevation (m MHW)", rotation=270)
 
         # add text and set title
-        ax1.text(0.5, 0.01, 'Ocean', c="white", horizontalalignment='center',
-         verticalalignment='bottom', transform=ax1.transAxes)
-        ax1.text(0.5, 0.98, 'Marsh/Bay', c="white", horizontalalignment='center',
-             verticalalignment='top', transform=ax1.transAxes)
+        ax1.text(
+            0.5, 0.01, 'Ocean', c="white", horizontalalignment='center',
+            verticalalignment='bottom', transform=ax1.transAxes)
+        ax1.text(
+            0.5, 0.98, 'Marsh/Bay', c="white", horizontalalignment='center',
+            verticalalignment='top', transform=ax1.transAxes)
         ax1.set_title("Year {0}".format(t))
 
         # set axis labels
@@ -233,21 +275,26 @@ def plot_domains_with_marsh_transect(casc_model, iB3D, save_dir, newfolder, buff
             ax1.set_ylabel("cross-shore distance (dam)")
 
         # transects on subplot 2 -----------------------------------
-        ax2 = plt.subplot2grid(shape=(nrows, ncols), loc=(nrows-1, 0), rowspan=1, colspan=ncols-1)
+        ax2 = plt.subplot2grid(shape=(nrows, ncols), loc=(nrows - 1, 0), rowspan=1, colspan=ncols - 1)
         # shape = (#rows, #cols), loc=(row index, col index), rowspan = # rows to cover
         # we want the transect to take up 1/3 of the figure space
-        ax2.plot(whole_transect_m1, c="black")
-        ax2.scatter(x_forest_m1, y_forest_m1, c="green")
-        ax2.scatter(x_marsh_m1, y_marsh_m1, c="brown")
-        ax2.set_ylim(-2.1, 4)
-        ax2.set_xlabel("Cross-shore Distance (m)")
-        ax2.set_ylabel("Elevation (m)")
+        ax2.plot(whole_transect_mhw, c="black")
+        ax2.scatter(x_forest_m1, y_forest_mhw, c="green")
+        ax2.scatter(x_marsh_m1, y_marsh_mhw, c="brown")
+        ax2.set_ylim(-3.1, 3)
+        if t == 0:
+            xlims = ax2.get_xlim()
+        ax2.set_xlim(xlims)
+        ax2.set_xlabel("cross-shore distance (m)")
+        ax2.set_ylabel("Elevation (m MHW)")
 
         # add text and set title
-        ax2.text(0.01, 0.05, 'Ocean', c="grey", horizontalalignment='left',
-         verticalalignment='center', transform=ax2.transAxes)
-        ax2.text(0.99, 0.05, 'Mainland', c="grey", horizontalalignment='right',
-             verticalalignment='center', transform=ax2.transAxes)
+        ax2.text(
+            0.01, 0.05, 'Ocean', c="grey", horizontalalignment='left',
+            verticalalignment='center', transform=ax2.transAxes)
+        ax2.text(
+            0.99, 0.05, 'Mainland', c="grey", horizontalalignment='right',
+            verticalalignment='center', transform=ax2.transAxes)
 
         plt.tight_layout()
 
