ec54ae929c5e8521bf982fafca3264bd282470b0
Lexi (Van Blunk) Fiegelist <lexifiegelist@gmail.com>
2026-07-30 14:06:19 -0400

    updated based on discussion with Laura. specific domains get different methods of breaking up the dunes and interior

--- scripts/marsh_ms/dune_topo_extractor_V2.py
+++ scripts/marsh_ms/dune_topo_extractor_V2.py
@@ -8,6 +8,7 @@ import os, copy
 from pathlib import Path
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy import stats
 
 
 def remove_water_cols(domain_array, w_elev):
@@ -171,7 +172,13 @@ def process_domain_file(
         arr_ocean_top = np.rot90(arr)
         dune_loc_array = np.array(dune_loc_array)
         max_dune_loc = np.nanmax(dune_loc_array)
+        min_dune_loc = np.nanmin(dune_loc_array)
+        mode_dune_loc = stats.mode(dune_loc_array).mode
         start_island = int(max_dune_loc + 1)
+        if "domain_3" in str(in_path):
+            start_island = int(min_dune_loc + 1)
+        elif "domain_19" in str(in_path):
+            start_island = int(mode_dune_loc + 1)
         topo_m = arr_ocean_top[start_island:, :]
 
     # remove rows that are all water cells
@@ -191,6 +198,8 @@ def process_domain_file(
         f"{dune_out.name} (len {dune_dm.size})"
         )
 
+    return topo_m, dune_m
+
 
 ############################################################################################
 # main script
@@ -198,10 +207,10 @@ def process_domain_file(
 plt.rcParams["font.size"] = 14
 
 # --- PATHS --------------------------------------------------------------
-version = "v6"  # save version to append to folder name
-LOAD_PATH = r"C:\Users\Lexi\Documents\UNC\data\DEM\processed_domains\rotated_domains_npys"
-TOPO_SAVE_PATH = r"C:\Users\Lexi\Documents\UNC\data\DEM\processed_domains\cascade_domains\topography\rotated_domains_{0}".format(version)
-DUNE_SAVE_PATH = r"C:\Users\Lexi\Documents\UNC\data\DEM\processed_domains\cascade_domains\dunes\rotated_domains_{0}".format(version)
+version = "v7"  # save version to append to folder name
+LOAD_PATH = r"C:\Users\agfig\OneDrive - University of North Carolina at Chapel Hill\UNC\data\DEM\processed_domains\rotated_domains_npys"
+TOPO_SAVE_PATH = r"C:\Users\agfig\OneDrive - University of North Carolina at Chapel Hill\UNC\data\DEM\processed_domains\cascade_domains\topography\rotated_domains_{0}_edited".format(version)
+DUNE_SAVE_PATH = r"C:\Users\agfig\OneDrive - University of North Carolina at Chapel Hill\UNC\data\DEM\processed_domains\cascade_domains\dunes\rotated_domains_{0}_edited".format(version)
 
 os.makedirs(TOPO_SAVE_PATH, exist_ok=True)
 os.makedirs(DUNE_SAVE_PATH, exist_ok=True)
@@ -210,13 +219,13 @@ os.makedirs(DUNE_SAVE_PATH, exist_ok=True)
 MHW_M = 0.421              # meters (NAVD88)
 BERM_ELEV_NAVD_M = 1.95    # meters (NAVD88)
 BEACH_START_THR_M = 0.5    # meters (MHW-relative), strict '>' comparison
-DUNE_WINDOW_PX = 10        # pixels
+DUNE_WINDOW_PX = 5         # pixels
 SENTINEL_WATER_M = -3.0    # meters (MHW-relative)
 TOPO_ROWS = 200            # number of inland rows to write
 ALONG_COLS = 50            # number of alongshore profiles
 OCEAN_LOC = "bottom"       # "top", "bottom", "left", or "right"
-shift_interior = True     # add cells to the beginning of the interior domain to keep it aligned
-use_const_interior = False  # select a start row for the interior (most landward dune cell + 1)
+shift_interior = False     # add cells to the beginning of the interior domain to keep it aligned
+use_const_interior = True  # select a start row for the interior (most landward dune cell + 1)
 
 load_dir = Path(LOAD_PATH)
 topo_dir = Path(TOPO_SAVE_PATH)
@@ -228,13 +237,13 @@ dune_dir.mkdir(parents=True, exist_ok=True)
 names = sorted(
     [
         n for n in os.listdir(load_dir)
-        if n.endswith(".npy") and n.startswith("domain_")
+        if n.endswith(".npy") and n.startswith("domain_21")
         ]
     )
 print(f"[info] Found {len(names)} domain file(s) in {load_dir}")
-
+topo_domain = []
 for name in names:
-    process_domain_file(
+    topo_domain, dune_domain = process_domain_file(
         load_dir / name,
         topo_dir,
         dune_dir,
@@ -250,4 +259,21 @@ for name in names:
         use_const_interior=use_const_interior
         )
 
+domain = np.vstack((dune_domain+(BERM_ELEV_NAVD_M - MHW_M), topo_domain))
+minz = -3
+maxz = 5
+xlabel = "alongshore distance (dam)"
+ylabel = "cross-shore distance (dam)"
+fig1 = plt.figure(figsize=[6,10])
+ax1 = fig1.add_subplot(111)
+mat1 = ax1.matshow(
+    domain,
+    cmap="terrain",
+    vmin=minz,
+    vmax=maxz,
+    )
+ax1.set_ylabel(ylabel)
+ax1.set_title("dune window: {0}".format(DUNE_WINDOW_PX))
+plt.gca().xaxis.tick_bottom()
+plt.show()
 
